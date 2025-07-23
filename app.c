539bed76d8a6ef71f30e24f83d57e4f502a79052
farid45219 <refrigerator.rni.eed7@waltonplc.com>
2025-07-23 10:28:40 +0600

    Modified: gpio toggle using assembly

--- app.c
+++ app.c
@@ -14,25 +14,32 @@ void App_Setup(void){
 	Debug_Init(38400);
 	GPIO_Init();
 	
-	global_var++;
-	Debug_Tx_Parameter_NL("before context save", global_var);
-	
-	Context_Save();
-	
-	global_var++;
-	
-	Debug_Tx_Parameter_NL("after context save", global_var);
-	
-	Context_Restore();
-	
-	Debug_Tx_Parameter_NL("after context restore", global_var);
-	
 	
 }
 
 void App_Main_Loop(void){
     
     Debug_Tx_Text_NL("Hello");
+	
+	//PORTC|=(1<<5);
+	
+	asm volatile (
+	  "LDS     R18,  0x28  \n\t"
+	  "LDI     R19,  0x20  \n\t"
+	  "ADD     R18,  R19   \n\t"
+	  "STS     0x28, R18   \n\t"
+	);
+	
 	_delay_ms(500);
+	
+	//PORTC&=~(1<<5);
+	
+	asm volatile (
+	  "LDS     R18,  0x28   \n\t"
+	  "LDI     R19,  0x20   \n\t"
+	  "SUB     R18,  R19    \n\t"
+	  "STS     0x28, R18    \n\t"
+	);
     
+	
 }
