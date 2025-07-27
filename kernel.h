7fc0af8400a11353294431ffe3ff20f32cfd3a61
farid132097 <faridmdislam@gmail.com>
2025-07-28 00:59:59 +0600

    Added: kernel tick val get function

--- kernel.h
+++ kernel.h
@@ -5,7 +5,7 @@
 //define kernel tick rate in Hz
 #define  KERNEL_TR 1000L
 
-extern void  Kernel_SysTick_Reg_Init(uint16_t tick_rate);
-
+extern void      Kernel_SysTick_Reg_Init(uint16_t tick_rate);
+extern uint64_t  Kernel_SysTick_Val_Get(void);
 
 #endif
