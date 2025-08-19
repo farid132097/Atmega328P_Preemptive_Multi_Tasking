b26995361c06d4f91690c373ca86f470b6df51fb
farid45219 <refrigerator.rni.eed7@waltonplc.com>
2025-08-19 15:18:44 +0600

    Modified: automated task_id inc assigned, working

--- main.c
+++ main.c
@@ -18,14 +18,14 @@ int main(void) {
 	Kernel_SysTick_Reg_Init(0x03, 0x82);
 	
 	Kernel_Task_Create(Tasks_Task0,  0);
-	Kernel_Task_Create(Tasks_Task2,  1);
-	Kernel_Task_Create(Tasks_Task3,  2);
-	Kernel_Task_Create(Tasks_Task4,  3);
-	Kernel_Task_Create(Tasks_Task5,  4);
-	Kernel_Task_Create(Tasks_Task6,  5);
-	Kernel_Task_Create(Tasks_Task7,  6);
-	Kernel_Task_Create(Tasks_Task8,  7);
-	Kernel_Task_Create(Tasks_Task1,  8);
+	Kernel_Task_Create(Tasks_Task2,  0);
+	Kernel_Task_Create(Tasks_Task3,  0);
+	Kernel_Task_Create(Tasks_Task4,  0);
+	Kernel_Task_Create(Tasks_Task5,  0);
+	Kernel_Task_Create(Tasks_Task6,  0);
+	Kernel_Task_Create(Tasks_Task7,  0);
+	Kernel_Task_Create(Tasks_Task8,  0);
+	Kernel_Task_Create(Tasks_Task1,  0);
 	
 	Kernel_Start_Tasks();
 	
