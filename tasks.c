7a705987595489b92235fc43bee8f8cd6a2da9a3
farid45219 <refrigerator.rni.eed7@waltonplc.com>
2025-09-08 19:23:47 +0600

    Added: Scheduler timeout working

--- tasks.c
+++ tasks.c
@@ -9,51 +9,53 @@
 void Tasks_Task0(void){
   
   Debug_Init(0);
-  
-  uint16_t val = 80;
-  
+  Kernel_Task_Sleep(5000);
   
   while(1){
     
 	  Debug_Tx_Byte(0);
-	  //Debug_Tx_Word_Conf(val--);
+    Debug_Tx_Word(Kernel_Task_Sleep_Time_Get(1));
+    Debug_Tx_Byte(0x20+Kernel_Task_Status_Get(1));
     Debug_Tx_Byte(0x10+Kernel_High_Prio_Task_ID_Get());
-    //Debug_Tx_Word(Kernel_Task_Sleep_Time_Get());
-	  //Kernel_Task_Sleep(100);
-	
+    Debug_Tx_Byte(Kernel_NTask_Get());
+    
   }
 }
 
 void Tasks_Task1(void){
   
-  Debug_Init(0);
   DDRD |= (1<<5);
-  uint16_t val = 100;
+
+  Debug_Init(0);
+  Kernel_Task_Sleep(4000);
   
   while(1){
     
     PORTD ^= (1<<5);
+
 	  Debug_Tx_Byte(1);
-	  //Debug_Tx_Word_Conf(val--);
+    Debug_Tx_Word(Kernel_Task_Sleep_Time_Get(2));
+    Debug_Tx_Byte(0x20+Kernel_Task_Status_Get(2));
     Debug_Tx_Byte(0x10+Kernel_High_Prio_Task_ID_Get());
-	  //Kernel_Task_Sleep(5);
 	
   }
 }
 
 void Tasks_Task2(void){
   
-  Debug_Init(0);
   DDRD |= (1<<6);
-  uint16_t val = 120;
+
+  Debug_Init(0);
+  Kernel_Task_Sleep(3000);
   
   while(1){
     
     PORTD ^= (1<<6);
+
 	  Debug_Tx_Byte(2);
-    //Debug_Tx_Word_Conf(val--);
+    Debug_Tx_Word(Kernel_Task_Sleep_Time_Get(3));
+    Debug_Tx_Byte(0x20+Kernel_Task_Status_Get(3));
     Debug_Tx_Byte(0x10+Kernel_High_Prio_Task_ID_Get());
-	  //Kernel_Task_Sleep(17);
 	
   }
 }
@@ -61,14 +63,14 @@ void Tasks_Task2(void){
 void Tasks_Task3(void){
   
   Debug_Init(0);
-  uint16_t val = 140;
+  Kernel_Task_Sleep(2000);
   
   while(1){
     
 	  Debug_Tx_Byte(3);
-	  //Debug_Tx_Word_Conf(val--);
+    Debug_Tx_Word(Kernel_Task_Sleep_Time_Get(4));
+    Debug_Tx_Byte(0x20+Kernel_Task_Status_Get(4));
     Debug_Tx_Byte(0x10+Kernel_High_Prio_Task_ID_Get());
-	  //Kernel_Task_Sleep(24);
 	
   }
 }
@@ -76,15 +78,15 @@ void Tasks_Task3(void){
 void Tasks_Task4(void){
   
   Debug_Init(0);
-  uint16_t val = 160;
+  Kernel_Task_Sleep(1000);
   
   while(1){
     
 	
 	  Debug_Tx_Byte(4);
-	  //Debug_Tx_Word_Conf(val--);
+    Debug_Tx_Word(Kernel_Task_Sleep_Time_Get(5));
+    Debug_Tx_Byte(0x20+Kernel_Task_Status_Get(5));
     Debug_Tx_Byte(0x10+Kernel_High_Prio_Task_ID_Get());
-	  //Kernel_Task_Sleep(26);
 	
   }
 }
