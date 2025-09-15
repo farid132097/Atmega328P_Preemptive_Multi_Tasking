01d42ed9861272fbbb1e3bd2e6985df58872b972
farid132097 <faridmdislam@gmail.com>
2025-09-16 00:13:32 +0600

    Added: all tasks, working, need to check interrupt disable while in thread context switch

--- main.c
+++ main.c
@@ -21,19 +21,11 @@ int main(void) {
 	Kernel_Task_Create(Tasks_Task3,  2);
 	Kernel_Task_Create(Tasks_Task4,  1);
 	Kernel_Task_Create(Tasks_Task5,  4);
+	Kernel_Task_Create(Tasks_Task6,  6);
+	Kernel_Task_Create(Tasks_Task7,  5);
+	Kernel_Task_Create(Tasks_Task8,  8);
+	Kernel_Task_Create(Tasks_Task9,  7);
 	
-    Debug_Init(0);
-    Debug_Tx_Byte_Conf(Kernel_Task_Prio_Get(0));
-    Debug_Tx_Byte_Conf(Kernel_Task_Prio_Get(1));
-	Debug_Tx_Byte_Conf(Kernel_Task_Prio_Get(2));
-	Debug_Tx_Byte_Conf(Kernel_Task_Prio_Get(3));
-	Debug_Tx_Byte_Conf(Kernel_Task_Prio_Get(4));
-	Debug_Tx_Byte_Conf(Kernel_Task_Prio_Get(5));
-	
-	Debug_Tx_Byte_Conf(Kernel_NTask_Get());
-    Debug_Tx_Byte_Conf(Kernel_High_Prio_Task_ID_Get());
-	Debug_Tx_Byte_Conf(Kernel_Abs_High_Prio_Task_ID_Get());
-    
 	Kernel_Start_Tasks();
 	
 	
