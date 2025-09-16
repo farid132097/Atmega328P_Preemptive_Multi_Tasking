47793b351934856d2f98e1063448102514d4ec2d
farid45219 <refrigerator.rni.eed7@waltonplc.com>
2025-09-16 18:05:42 +0600

    Added: printing->cpu usage in each task

--- tasks.c
+++ tasks.c
@@ -16,12 +16,18 @@ void Tasks_Task1(void){
     
     PORTC ^= (1<<1);
 
-	  Debug_Tx_Byte(1);
+	  /*Debug_Tx_Byte(1);
     Debug_Tx_Word(Kernel_Task_Sleep_Time_Get(1));
     Debug_Tx_Byte(0x20+Kernel_Task_Status_Get(1));
     Debug_Tx_Byte(0x10+Kernel_Abs_High_Prio_Task_ID_Get());
     Debug_Tx_Byte(Kernel_Lowest_Prio_Get());
-    Debug_Tx_Byte(Kernel_CPU_Usage_Get());
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');*/
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()/10) + 48);
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()%10) + 48);
+    Debug_Tx_Byte('%');
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');
     Kernel_Task_Sleep(10);
     
   }
@@ -37,12 +43,18 @@ void Tasks_Task2(void){
     
     PORTC ^= (1<<2);
 
-	  Debug_Tx_Byte(2);
+	  /*Debug_Tx_Byte(2);
     Debug_Tx_Word(Kernel_Task_Sleep_Time_Get(2));
     Debug_Tx_Byte(0x20+Kernel_Task_Status_Get(2));
     Debug_Tx_Byte(0x10+Kernel_Abs_High_Prio_Task_ID_Get());
     Debug_Tx_Byte(Kernel_Lowest_Prio_Get());
-    Debug_Tx_Byte(Kernel_CPU_Usage_Get());
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');*/
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()/10) + 48);
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()%10) + 48);
+    Debug_Tx_Byte('%');
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');
     Kernel_Task_Sleep(20);
 	
   }
@@ -58,12 +70,18 @@ void Tasks_Task3(void){
     
     PORTC ^= (1<<3);
 
-	  Debug_Tx_Byte(3);
+	  /*Debug_Tx_Byte(3);
     Debug_Tx_Word(Kernel_Task_Sleep_Time_Get(3));
     Debug_Tx_Byte(0x20+Kernel_Task_Status_Get(3));
     Debug_Tx_Byte(0x10+Kernel_Abs_High_Prio_Task_ID_Get());
     Debug_Tx_Byte(Kernel_Lowest_Prio_Get());
-    Debug_Tx_Byte(Kernel_CPU_Usage_Get());
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');*/
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()/10) + 48);
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()%10) + 48);
+    Debug_Tx_Byte('%');
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');
     Kernel_Task_Sleep(30);
 	
   }
@@ -79,12 +97,18 @@ void Tasks_Task4(void){
     
     PORTC ^= (1<<4);
 
-	  Debug_Tx_Byte(4);
+	  /*Debug_Tx_Byte(4);
     Debug_Tx_Word(Kernel_Task_Sleep_Time_Get(4));
     Debug_Tx_Byte(0x20+Kernel_Task_Status_Get(4));
     Debug_Tx_Byte(0x10+Kernel_Abs_High_Prio_Task_ID_Get());
     Debug_Tx_Byte(Kernel_Lowest_Prio_Get());
-    Debug_Tx_Byte(Kernel_CPU_Usage_Get());
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');*/
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()/10) + 48);
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()%10) + 48);
+    Debug_Tx_Byte('%');
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');
     Kernel_Task_Sleep(40);
 	
   }
@@ -100,12 +124,18 @@ void Tasks_Task5(void){
     
     PORTC ^= (1<<5);
 
-	  Debug_Tx_Byte(5);
+	  /*Debug_Tx_Byte(5);
     Debug_Tx_Word(Kernel_Task_Sleep_Time_Get(5));
     Debug_Tx_Byte(0x20+Kernel_Task_Status_Get(5));
     Debug_Tx_Byte(0x10+Kernel_Abs_High_Prio_Task_ID_Get());
     Debug_Tx_Byte(Kernel_Lowest_Prio_Get());
-    Debug_Tx_Byte(Kernel_CPU_Usage_Get());
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');*/
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()/10) + 48);
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()%10) + 48);
+    Debug_Tx_Byte('%');
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');
     Kernel_Task_Sleep(50);
 	
   }
@@ -117,12 +147,18 @@ void Tasks_Task6(void){
   
   while(1){
     
-    Debug_Tx_Byte(6);
+    /*Debug_Tx_Byte(6);
     Debug_Tx_Word(Kernel_Task_Sleep_Time_Get(6));
     Debug_Tx_Byte(0x20+Kernel_Task_Status_Get(6));
     Debug_Tx_Byte(0x10+Kernel_Abs_High_Prio_Task_ID_Get());
     Debug_Tx_Byte(Kernel_Lowest_Prio_Get());
-    Debug_Tx_Byte(Kernel_CPU_Usage_Get());
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');*/
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()/10) + 48);
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()%10) + 48);
+    Debug_Tx_Byte('%');
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');
     Kernel_Task_Sleep(60);
 	
   }
@@ -134,12 +170,18 @@ void Tasks_Task7(void){
   
   while(1){
     
-	  Debug_Tx_Byte(7);
+	  /*Debug_Tx_Byte(7);
     Debug_Tx_Word(Kernel_Task_Sleep_Time_Get(7));
     Debug_Tx_Byte(0x20+Kernel_Task_Status_Get(7));
     Debug_Tx_Byte(0x10+Kernel_Abs_High_Prio_Task_ID_Get());
     Debug_Tx_Byte(Kernel_Lowest_Prio_Get());
-    Debug_Tx_Byte(Kernel_CPU_Usage_Get());
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');*/
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()/10) + 48);
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()%10) + 48);
+    Debug_Tx_Byte('%');
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');
     Kernel_Task_Sleep(70);
 	
   }
@@ -151,12 +193,18 @@ void Tasks_Task8(void){
   
   while(1){
     
-	  Debug_Tx_Byte(8);
+	  /*Debug_Tx_Byte(8);
     Debug_Tx_Word(Kernel_Task_Sleep_Time_Get(8));
     Debug_Tx_Byte(0x20+Kernel_Task_Status_Get(8));
     Debug_Tx_Byte(0x10+Kernel_Abs_High_Prio_Task_ID_Get());
     Debug_Tx_Byte(Kernel_Lowest_Prio_Get());
-    Debug_Tx_Byte(Kernel_CPU_Usage_Get());
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');*/
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()/10) + 48);
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()%10) + 48);
+    Debug_Tx_Byte('%');
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');
     Kernel_Task_Sleep(80);
 	
   }
@@ -168,12 +216,18 @@ void Tasks_Task9(void){
   
   while(1){
     
-	  Debug_Tx_Byte(9);
+	  /*Debug_Tx_Byte(9);
     Debug_Tx_Word(Kernel_Task_Sleep_Time_Get(9));
     Debug_Tx_Byte(0x20+Kernel_Task_Status_Get(9));
     Debug_Tx_Byte(0x10+Kernel_Abs_High_Prio_Task_ID_Get());
     Debug_Tx_Byte(Kernel_Lowest_Prio_Get());
-    Debug_Tx_Byte(Kernel_CPU_Usage_Get());
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');*/
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()/10) + 48);
+    Debug_Tx_Byte( (Kernel_CPU_Usage_Get()%10) + 48);
+    Debug_Tx_Byte('%');
+    Debug_Tx_Byte('\r');
+    Debug_Tx_Byte('\n');
     Kernel_Task_Sleep(90);
 	
   }
